21dcea9e9735a07f74d5d1657d342b05af67ebcc
Jayasurya.K <jayasurya7aa@gmail.com>
2023-02-23 23:01:45 +0530

    added updates

--- src/App.js
+++ src/App.js
@@ -3,22 +3,62 @@ import { useState, useEffect } from "react";
 import Axios from "axios";
 
 function App() {
+  const [listOfUsers, setListOfUsers] = useState([]);
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState(0);
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/getUsers").then((response) => {
+      setListOfUsers(response.data);
+    });
+  }, []);
+
+  const createUser = () => {
+    Axios.post("http://localhost:3001/createUser", {
+      name,
+      phone
+    }).then((response) => {
+      setListOfUsers([
+        ...listOfUsers,
+        {
+          name,
+          phone
+        },
+      ]);
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="usersDisplay">
+        {listOfUsers.map((user) => {
+          return (
+            <div>
+              <h1>Name: {user.name}</h1>
+              <h1>Phone: {user.phone}</h1>
+            </div>
+          );
+        })}
+      </div>
+
+      <div>
+        <input
+          type="text"
+          placeholder="Name..."
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+        <input
+          type="number"
+          placeholder="Phone..."
+          onChange={(event) => {
+            setPhone(event.target.value);
+          }}
+        />
+        <button onClick={createUser}> Create User </button>
+        
+      </div>
     </div>
   );
 }
